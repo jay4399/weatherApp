0ac38173e3b911efde3a5015c5dab1125afdba3e
Jay Rathod <Jay@Jays-MacBook-Pro.local>
2020-10-18 15:52:56 +0400

    Updates

---
+++ precache-manifest.85ab48f1ee6fbab4f60dffc5bd2937bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a47fe52bd115c43bc4d396976303b5f",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "bd8e001416d1ae66a4e4",
+    "url": "/weatherApp/static/css/main.9a5d1ddb.chunk.css"
+  },
+  {
+    "revision": "f46fa7c696025d8a90f1",
+    "url": "/weatherApp/static/js/2.33c4f7da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherApp/static/js/2.33c4f7da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd8e001416d1ae66a4e4",
+    "url": "/weatherApp/static/js/main.cf253bce.chunk.js"
+  },
+  {
+    "revision": "fcc70ecd800c4378ffa3",
+    "url": "/weatherApp/static/js/runtime-main.57c99f3c.js"
+  },
+  {
+    "revision": "80e19d92838adbc8905385af48999139",
+    "url": "/weatherApp/static/media/bg.80e19d92.jpg"
+  }
+]);
